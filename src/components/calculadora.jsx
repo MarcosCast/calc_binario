36b596e99ef4dbeb5d92eb0e24c6f81e80abd2f7
MarcosCast <marcosmax.5@gmail.com>
2023-01-04 22:09:24 -0300

    Erro com CSS

--- src/components/calculadora.jsx
+++ src/components/calculadora.jsx
@@ -1,2 +1,11 @@
 import './calculadora.scss';
 
+function Calculator() {
+    return(
+        <div className="calc">
+            <h1>Calculadora binária</h1>
+        </div>
+    );
+}
+
+export default Calculator;
