082a6c6b355eddb00f8c5525a1127444c70099b2
MarcosCast <marcosmax.5@gmail.com>
2023-01-09 18:33:22 -0300

    Corpo do conversor

--- local/src/components/Calculadora.jsx
+++ local/src/components/Calculadora.jsx
@@ -1,9 +1,17 @@
 import './Calculadora.scss';
+import React from 'react';
+import { useState } from 'react';
 
 function Conversor() {
     return(
         <div className="conversor">
-            Hello World
+            <div className="input">
+                <input type="text" className='numero_binario' placeholder='Número Binário'
+                 />
+
+                <input type="text" className='numero_decimal' placeholder='Número Decimal' 
+                  />
+            </div>
         </div>
     );
 }
